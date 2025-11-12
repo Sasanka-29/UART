f1987044c89dae75fdc523d090a84f176840d580
Sasanka <sasankabarman2016@gmail.com>
2025-11-12 11:58:51 +0530

    Initial

--- RandomValueDetection.py
+++ RandomValueDetection.py
@@ -1,18 +1,18 @@
 import serial
 import time
 
-# Same reference table
-ref_list = ['A','F','K','3','Z','Q','L','9','M','2']
+ref_list = ['A', 'F', 'K', '3', 'Z', 'Q', 'L', '9', 'M', '2']
 
-# Configure port (change COM port as per your system)
-ser = serial.Serial('COM28', 9600, timeout=1)
+ser = serial.Serial('COM28', 9600)
 time.sleep(2)
 
-def request_char(index):
-    ser.write(f"{index}\n".encode())
+
+def request_char(index):  #sends index to MSP and returns a reply
+    ser.write(f"{index}\n".encode())  #converts index into a string and sends it to MSP
     time.sleep(0.1)
     response = ser.readline().decode().strip()
     return response
+
 
 while True:
     idx = input("Enter index (0-9) to request or x to exit: ")
@@ -20,12 +20,12 @@ while True:
     if idx.lower() == 'x':
         break
 
-    if not idx.isdigit() or not (0 <= int(idx) <= 9):
+    if not idx.isdigit() or not (0 <= int(idx) <= 9):  #Prevent non-integer inputs and then converts string to int
         print("Invalid input, enter 0-9")
         continue
 
-    received = request_char(idx)
-    expected = ref_list[int(idx)]
+    received = request_char(idx)  #Calls request_char function to send an index to MSP and get a reply
+    expected = ref_list[int(idx)]  #looks up the expected value in ref_list
 
     print("MSP430 replied :", received)
     print("Expected       :", expected)
